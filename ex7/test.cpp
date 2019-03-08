fc203463196605ed3b9633c90feae5ce57fc3802
Toshihiro KONDA <kontoshi0317@gmail.com>
2019-03-09 08:53:16 +0900

    lli test

---
+++ ex7/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+int main(int argc, char** argv) {
+  for (int i=1; i<10; i++) {
+    std::cout << fibonacci(i) << std::endl;
+  }
+}
